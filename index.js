5a1e99620f0457e89faca31be2baf341cbddddf4
Nonchalantcode <hectorg269s@outlook.com>
2023-06-10 08:27:50 -0600

    modified code to add validators in mongoose

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const Note = require("./models/note.js");
 
+var requestLogger = (request, response, next) => {
+  console.log({
+    "request method": request.method,
+    "request url": request.url,
+  });
+  next();
+};
+
+app.use(express.static("build"));
 app.use(express.json());
+app.use(requestLogger);
 app.use(cors());
 
-const requestLogger = (request, response, next) => {
+/* const requestLogger = (request, response, next) => {
   console.log("Method: ", request.method);
   console.log("Path: ", request.path);
   console.log("Body: ", request.body);
   console.log("------");
   next();
+}; */
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(404).send({ error: "malformatted id." });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+  next(error);
 };
 
-app.use(requestLogger);
-app.use(express.static("build"));
+var unknownEndpoint = (request, response) => {
+  console.log("The route: ", request.route, " does not exist");
+  response.status(404).send({ error: "unkown endpoint" });
+};
 
 let notes = [
   { id: 1, content: "HTML is easy", important: true },
@@ -22,67 +47,70 @@ let notes = [
   { id: 3, content: "GET and POST are the most important methods of HTTP protocol", important: true },
 ];
 
-app.get("/", (request, response) => {
-  console.log("using defualt rout");
-  response.send("<h1>Hello world!</h1>");
-});
-
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
-});
-
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  console.log({ id });
-  const note = notes.find((note) => {
-    console.log(note.id, typeof note.id, id, typeof id, note.id === id);
-    return note.id === id;
+  Note.find({}).then((notes) => {
+    response.json(notes);
   });
-  console.log({ note });
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => {
-    return note.id !== id;
-  });
-  response.status(204).end();
-});
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
-app.post("/api/notes", (request, response) => {
-  const body = request.body;
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
     });
-  }
-  const note = {
+});
+
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.post("/api/notes", (request, response, next) => {
+  const body = request.body;
+
+  const note = new Note({
     content: body.content,
     important: !!body.important,
-    id: generateId(),
-  };
-  notes = notes.concat(note);
-  response.json(note);
+  });
+
+  note
+    .save()
+    .then((savedNote) => {
+      console.log({ savedNote });
+      response.json(savedNote);
+    })
+    .catch((err) => next(err));
 });
 
-const unknownEndpoint = (request, response) => {
-  console.log("The route: ", request.route, " does not exist");
-  response.status(404).send({ error: "unkown endpoint" });
-};
+app.put("/api/notes/:id", (request, response, next) => {
+  const { content, important } = request.body;
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
 
 app.use(unknownEndpoint);
+// app.use(unknownEndpoint);
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
